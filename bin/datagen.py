1fb54bc29eff176116e2e36bbfac3db13a4f02bd
juandavm <juanda.vm@gmail.com>
2013-06-13 21:07:15 +0200

    Fixed a bug into the random data generator.

--- bin/datagen.py
+++ bin/datagen.py
@@ -9,19 +9,16 @@ def write_cache(actual,samples,cache,file):
 
 # Generate random Gaussian numbers based on the distributions.
 def generate(samples,dimensions,distributions,file):
-	file.write(str(dimensions)+" "+str(samples)+"\n")
-	count,cache=0,[]
+	count,cache=write_cache(0,samples,[str(dimensions)+" "+str(samples)],file)
 	for i in range(1,samples+1):
 		distribution,numbers=random.choice(distributions),[]
 		for j in range(dimensions):
 			m,v=distribution[j]
 			numbers.append(("%.3f"%random.gauss(m,v)))
-		# Use a cache to speedup the write on disk.
-		if count>=100000:
+		if count>=100000: # Use a cache to speedup the write on disk.
 			count,cache=write_cache(i,samples,cache,file)
-		else:
-			count+=dimensions
-			cache.append(" ".join(numbers))
+		count+=dimensions
+		cache.append(" ".join(numbers))
 	count,cache=write_cache(samples,samples,cache,file)
 	sys.stdout.write("\n")
 
@@ -31,8 +28,7 @@ def generate_distributions(samples,dimensions):
 	for i in range(numdist):
 		distdim=[]
 		for j in range(dimensions):
-			m=random.uniform(-10000,10000)
-			v=random.uniform(10,100)
+			m,v=random.uniform(-10000,10000),random.uniform(10,100)
 			distdim.append((m,v))
 		distributions.append(distdim)
 	return distributions
@@ -42,13 +38,11 @@ def main():
 	if len(sys.argv)<4:
 		sys.stderr.write("Usage: "+sys.argv[0]+" <dimensions> <samples> <file.txt>\n")
 	else:
-		dimensions=int(sys.argv[1])
-		samples=int(sys.argv[2])
+		dimensions,samples=int(sys.argv[1]),int(sys.argv[2])
 		file=open(str(sys.argv[3]),"w")
 		distributions=generate_distributions(samples,dimensions)
 		generate(samples,dimensions,distributions,file)
 		file.close()
 
 # If is not imported as a library, execute the main function.
-if __name__=="__main__":
-	main()
+if __name__=="__main__": main()
